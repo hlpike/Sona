66e2857d3dcacefd6c642370e3b1d992afe9726b
hlpike <hhlpike@hotmail.com>
2020-02-02 06:40:31 -0800

    Added sign out button listener

--- app/src/main/java/com/teammh/sona/view/MainActivity.java
+++ app/src/main/java/com/teammh/sona/view/MainActivity.java
@@ -100,6 +100,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
     }
 
     public void updateUI() {
