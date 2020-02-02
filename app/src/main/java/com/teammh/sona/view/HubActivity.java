488b6e6fad954038a79ce14eb5c8c265a83d84ed
geisy <gvald071@fiu.edu>
2020-02-02 03:05:22 -0500

    references

--- app/src/main/java/com/teammh/sona/view/HubActivity.java
+++ app/src/main/java/com/teammh/sona/view/HubActivity.java
@@ -3,14 +3,28 @@ package com.teammh.sona.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.teammh.sona.R;
 
 public class HubActivity extends AppCompatActivity {
 
+    private Button feelingButton;
+    private Button homeButton;
+    private Button profileButton;
+    private Button infoButton;
+    private ImageView hubImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hub);
+
+        feelingButton = (Button)findViewById(R.id.hub_about_day2);
+        homeButton = (Button)findViewById(R.id.hub_home2);
+        profileButton = (Button)findViewById(R.id.hub_profile2);
+        infoButton = (Button)findViewById(R.id.hub_info2);
+        hubImage = (ImageView)findViewById(R.id.imageView2);
     }
 }
